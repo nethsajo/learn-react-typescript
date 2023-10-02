d71e9ede4257b57cf81bc99fbabc9c99ea3052e8
Jan Kenneth Sajo <nethsajo98@gmail.com>
2023-10-02 15:22:14 +0800

    update far away header and add form

--- 2. State, Events, and Forms (Interactive Components)/Far Away/src/components/FarAway/components/Form.tsx
+++ 2. State, Events, and Forms (Interactive Components)/Far Away/src/components/FarAway/components/Form.tsx
@@ -1,7 +1,54 @@
+import { type ChangeEvent, useState } from 'react';
+
 export function Form() {
+  const [item, setItem] = useState('');
+  const [quantity, setQuantity] = useState(1);
+
+  const handleItem = (e: ChangeEvent<HTMLInputElement>) => {
+    setItem(e.target.value);
+  };
+
+  const handleQuantity = (e: ChangeEvent<HTMLSelectElement>) => {
+    setQuantity(Number(e.target.value));
+  };
+
   return (
-    <div className="flex items-center justify-center bg-amber-700 py-6">
-      <h3 className="text-center text-amber-50">What do you need for your trip? </h3>
+    <div className="flex flex-col items-center justify-center space-y-4 bg-amber-700 px-4 py-6 md:flex-row md:space-x-6 md:space-y-0">
+      <h3 className="text-center text-base text-amber-50 md:text-xl">
+        What do you need for your trip?
+      </h3>
+      <form>
+        <div className="flex flex-col items-center space-y-4 md:flex-row md:space-x-6 md:space-y-0">
+          <select
+            name="quantity"
+            value={quantity}
+            onChange={handleQuantity}
+            className="w-full rounded-md bg-amber-50 px-4 py-2 font-medium"
+          >
+            {Array.from({ length: 20 }, (_, index) => {
+              return (
+                <option key={index} value={index + 1}>
+                  {index + 1}
+                </option>
+              );
+            })}
+          </select>
+          <input
+            type="text"
+            name="item"
+            value={item}
+            onChange={handleItem}
+            className="appearance-none rounded-md bg-amber-50 px-4 py-2 font-medium text-amber-800 placeholder:text-gray-400"
+            placeholder="Add item..."
+          />
+          <button
+            type="submit"
+            className="w-full rounded-md bg-emerald-500 px-6 py-2 font-semibold text-emerald-50"
+          >
+            Add
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
