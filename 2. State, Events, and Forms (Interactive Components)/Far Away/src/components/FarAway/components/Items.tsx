627748e3d3ff66ccc9dc176543462f6723b8913a
Jan Kenneth Sajo <nethsajo98@gmail.com>
2023-10-04 13:14:51 +0800

    update the items component of far away

--- 2. State, Events, and Forms (Interactive Components)/Far Away/src/components/FarAway/components/Items.tsx
+++ 2. State, Events, and Forms (Interactive Components)/Far Away/src/components/FarAway/components/Items.tsx
@@ -1,7 +1,8 @@
-import { type Item } from './Item.types';
+import { Item } from './Item';
+import { type Item as ItemType } from './Item.types';
 
 type Props = {
-  items: Item[];
+  items: ItemType[];
   onDeleteItem: (id: number) => void;
 };
 
@@ -9,47 +10,9 @@ export function Items({ items, onDeleteItem }: Props) {
   return (
     <div className="w-full bg-yellow-900 py-8">
       <div className="max-h-[440px] max-w-2xl overflow-y-auto px-4 sm:mx-auto">
-        <div className="flex flex-col gap-4 text-amber-50">
+        <div className="flex flex-col space-y-4 text-amber-50">
           {items.map(item => {
-            return (
-              <div
-                key={item.id}
-                className="flex justify-between rounded-md bg-yellow-800 px-4 py-3"
-              >
-                <div className="grid flex-1 grid-cols-[24px_1fr] items-start justify-center gap-x-4">
-                  <input
-                    type="checkbox"
-                    id={`item__${item.id}`}
-                    value={String(item.packed)}
-                    className="row-span-2 mt-1 h-6 w-6 rounded-sm border-slate-300 bg-transparent text-amber-600 transition duration-150 focus:ring focus:ring-amber-500 focus:ring-offset-1"
-                  />
-                  <label
-                    htmlFor={`item__${item.id}`}
-                    className="col-start-2 row-start-1 mb-1 text-lg leading-none sm:text-xl"
-                  >
-                    {item.item}
-                  </label>
-                  <p className="col-start-2 row-start-2">x {item.quantity}</p>
-                </div>
-                <button
-                  onClick={() => onDeleteItem(item.id)}
-                  className="mt-1 inline-flex self-start whitespace-nowrap rounded-md bg-transparent p-0.5 text-slate-200 transition duration-300 hover:bg-amber-600 active:bg-amber-700"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="h-6 w-6 fill-current"
-                  >
-                    <path d="M18 6 6 18" />
-                    <path d="m6 6 12 12" />
-                  </svg>
-                </button>
-              </div>
-            );
+            return <Item key={item.id} item={item} onDeleteItem={onDeleteItem} />;
           })}
         </div>
       </div>
