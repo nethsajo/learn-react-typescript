242356dac8fe8575816fdc8f7f2ed82ad6262fa5
Jan Kenneth Sajo <nethsajo98@gmail.com>
2023-10-11 15:08:53 +0800

    add eat-n-split initial files

---
+++ Projects/Eat 'N Split/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: 'Inter, sans-serif',
+      },
+      keyframes: {
+        'accordion-up': {
+          from: { height: 'var(--accordion-content-height)' },
+          to: { height: 0 },
+        },
+        'accordion-down': {
+          from: { height: 0 },
+          to: { height: 'var(--accordion-content-height)' },
+        },
+      },
+      animation: {
+        'accordion-up': 'accordion-up 200ms ease-out',
+        'accordion-down': 'accordion-down 200ms ease-out',
+      },
+    },
+  },
+  plugins: [require('@tailwindcss/forms')],
+};
