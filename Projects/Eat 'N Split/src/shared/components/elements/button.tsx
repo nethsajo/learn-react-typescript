c4a6c4c8de3ec5baabd5278810b5b766fb03e328
Jan Kenneth Sajo <nethsajo98@gmail.com>
2023-10-17 15:24:22 +0800

    update button component of eat-n-split

--- Projects/Eat 'N Split/src/shared/components/elements/button.tsx
+++ Projects/Eat 'N Split/src/shared/components/elements/button.tsx
@@ -2,7 +2,7 @@ import type React from 'react';
 
 export type ButtonProps = {
   type?: 'button' | 'submit' | 'reset';
-  size: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
   color?: 'primary' | 'secondary' | 'success' | 'danger';
   variant?: 'text' | 'outlined' | 'contained';
   onClick: () => void;
@@ -16,8 +16,25 @@ export function Button({
   children,
   onClick,
 }: React.PropsWithChildren<ButtonProps>) {
+  const sizes = {
+    xs: 'px-2 py-1',
+    sm: 'px-3 py-2',
+    md: 'px-4 py-3',
+    lg: 'px-5 py-4',
+    xl: 'px-6 py-5',
+  };
+
+  const colors = {
+    primary: 'bg-blue-500',
+    secondary: 'bg-gray-100',
+    success: 'bg-teal-500',
+    danger: 'bg-red-500',
+  };
+
+  const classes = `${sizes[size]} ${colors[color]}`;
+
   return (
-    <button type={type} onClick={onClick}>
+    <button type={type} onClick={onClick} className={classes}>
       {children}
     </button>
   );
