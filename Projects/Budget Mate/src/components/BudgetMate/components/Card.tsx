0b5180e28901b0d3ca47a35888da578883b23df7
Jan Kenneth Sajo <nethsajo98@gmail.com>
2023-11-07 15:17:54 +0800

    add header and cards in budget mate

---
+++ Projects/Budget Mate/src/components/BudgetMate/components/Card.tsx
@@ -0,0 +1,3 @@
+export function Card() {
+  return <div className="rounded-md bg-white p-4 shadow-sm sm:p-6">Card</div>;
+}
--- Projects/Budget Mate/src/components/BudgetMate/index.tsx
+++ Projects/Budget Mate/src/components/BudgetMate/index.tsx
@@ -1,3 +1,16 @@
+import { Card } from './components/Card';
+
 export default function BudgetMate() {
-  return <div>BudgetMate</div>;
+  return (
+    <div className="mx-auto mt-24 max-w-none px-4 sm:px-6 md:max-w-6xl">
+      <h1 className="mb-8 text-center text-2xl font-bold tracking-tight text-slate-500 sm:text-4xl">
+        Budget Mate
+      </h1>
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-3 sm:gap-6">
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  );
 }
