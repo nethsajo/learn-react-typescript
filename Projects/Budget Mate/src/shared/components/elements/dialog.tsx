4fe2a569300f64357cbc327639c0d84cf0de90ca
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2023-12-08 16:15:09 +0800

    fixed the error in transition

--- Projects/Budget Mate/src/shared/components/elements/dialog.tsx
+++ Projects/Budget Mate/src/shared/components/elements/dialog.tsx
@@ -1,5 +1,5 @@
 import { X } from 'lucide-react';
-import React from 'react';
+import React, { useRef } from 'react';
 import { useContext, useState } from 'react';
 import { createPortal } from 'react-dom';
 import { twJoin, twMerge } from 'tailwind-merge';
@@ -67,11 +67,14 @@ const DialogOverlay = () => {
 const DialogContent = ({ children, name }: DialogContentProps) => {
   const context = useContext(DialogContext);
 
+  const nodeRef = useRef(null);
+
   return (
-    <Transition in={Boolean(context.dialogName)} timeout={100} unmountOnExit>
+    <Transition nodeRef={nodeRef} in={Boolean(context.dialogName)} timeout={100} unmountOnExit>
       <DialogPortal>
         <DialogOverlay />
         <div
+          ref={nodeRef}
           role="dialog"
           data-state={getState(context.dialogName)}
           className="fixed left-[50%] top-[50%] z-50 grid w-full max-w-2xl translate-x-[-50%] translate-y-[-50%] gap-4 border bg-white px-4 py-8 shadow-lg duration-300 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:max-w-[525px] sm:rounded-lg sm:px-6"
