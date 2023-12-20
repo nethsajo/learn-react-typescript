4c04cdaf9600e8c550ab10777c58c92f3bd51f0a
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2023-12-20 22:54:18 +0800

    fix the alignment of edit icon in budget mate project

--- Projects/Budget Mate/src/components/BudgetMate/components/Budget.tsx
+++ Projects/Budget Mate/src/components/BudgetMate/components/Budget.tsx
@@ -31,48 +31,50 @@ export function Budget({ budget, onSetBudget }: Props) {
 
   return (
     <Card>
-      <div className="flex items-center space-x-4">
+      <div className="flex items-center">
         <div className="inline-flex h-12 w-12 items-center justify-center rounded-full bg-blue-50 text-blue-500">
           <CircleDollarSign className="h-6 w-6 stroke-current" />
         </div>
-        <div className="flex flex-1 flex-col">
+        <div className="ml-4 flex flex-1 flex-col">
           <h2 className="text-xl font-semibold tracking-tight text-slate-600">
             &#8369; {formatNumber(budget)}
           </h2>
           <span className="text-sm font-medium text-slate-400">Budget</span>
         </div>
-        <Dialog>
-          <Dialog.Trigger>
-            <Button color="none">
-              <Pencil className="h-6 w-6 stroke-slate-500 transition-colors hover:stroke-slate-600" />
-            </Button>
-          </Dialog.Trigger>
-          <Dialog.Content>
-            <form onSubmit={handleSubmit}>
-              <Dialog.Header>
-                <Dialog.Title>Edit budget</Dialog.Title>
-                <Dialog.Description>
-                  Customize your budget effortlessly. Click update to ensure your financial plan is
-                  perfectly tailored to your needs
-                </Dialog.Description>
-              </Dialog.Header>
-              <div className="py-6">
-                <Input
-                  id="budget"
-                  label="Enter your budget"
-                  value={amount}
-                  onChange={handleAmount}
-                  className="rounded-md py-2"
-                />
-              </div>
-              <Dialog.Footer>
-                <Button type="submit" size="lg">
-                  Update changes
-                </Button>
-              </Dialog.Footer>
-            </form>
-          </Dialog.Content>
-        </Dialog>
+        <div className="inline-flex items-center justify-center self-stretch sm:mx-4">
+          <Dialog>
+            <Dialog.Trigger>
+              <Button color="none">
+                <Pencil className="h-6 w-6 stroke-slate-500 transition-colors hover:stroke-slate-600" />
+              </Button>
+            </Dialog.Trigger>
+            <Dialog.Content>
+              <form onSubmit={handleSubmit}>
+                <Dialog.Header>
+                  <Dialog.Title>Edit budget</Dialog.Title>
+                  <Dialog.Description>
+                    Customize your budget effortlessly. Click update to ensure your financial plan
+                    is perfectly tailored to your needs
+                  </Dialog.Description>
+                </Dialog.Header>
+                <div className="py-6">
+                  <Input
+                    id="budget"
+                    label="Enter your budget"
+                    value={amount}
+                    onChange={handleAmount}
+                    className="rounded-md py-2"
+                  />
+                </div>
+                <Dialog.Footer>
+                  <Button type="submit" size="lg">
+                    Update changes
+                  </Button>
+                </Dialog.Footer>
+              </form>
+            </Dialog.Content>
+          </Dialog>
+        </div>
       </div>
     </Card>
   );
