e95cb34e7ce695ec5c0f21ab8a78a6a882f73749
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2023-12-22 22:12:30 +0800

    update the expense item of budget mate

--- Projects/Budget Mate/src/components/BudgetMate/components/Expenses.tsx
+++ Projects/Budget Mate/src/components/BudgetMate/components/Expenses.tsx
@@ -1,4 +1,4 @@
-import { Button } from 'shared/components/elements/button';
+import { X, Tag } from 'lucide-react';
 import { formatNumber } from 'shared/utils/commons';
 
 import { type Expense } from './types';
@@ -20,29 +20,28 @@ export function Expenses({ expenses, onRemoveExpense }: Props) {
 
   return (
     <>
-      <h2 className="mb-3 font-semibold text-slate-600">
-        Your {expenses.length > 1 ? 'expenses' : 'expense'}
-      </h2>
+      <h2 className="mb-3 font-semibold text-slate-600">Spending Insights</h2>
       {expenses.length > 0 ? (
-        <div className="grid gap-4">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
           {expenses.map(expense => {
             return (
-              <div key={expense.id} className="flex rounded-md bg-white px-4 py-2 shadow-sm">
-                <div className="grid w-full grid-cols-2 items-center gap-2 sm:grid-cols-[auto_1fr_auto_auto]">
+              <div key={expense.id} className="relative rounded-md bg-white px-4 py-3 shadow-sm">
+                <div className="space-y-2">
                   <p className="font-bold text-slate-600">{expense.item}</p>
-                  <span className="text-sm">{formatDate(expense.date)}</span>
-                  <span className="text-sm font-medium text-slate-500">
-                    &#8369; {formatNumber(expense.cost)}
-                  </span>
-                  <Button
-                    color="danger"
-                    size="xs"
-                    className="self-end"
-                    onClick={() => onRemoveExpense(expense.id)}
-                  >
-                    Remove
-                  </Button>
+                  <p className="text-sm text-slate-400">{formatDate(expense.date)}</p>
+                  <div className="inline-flex items-center space-x-1 rounded-full bg-blue-500 px-2 py-1 text-white">
+                    <Tag className="h-4 w-4 stroke-current" />
+                    <span className="text-xs font-medium ">
+                      &#8369; {formatNumber(expense.cost)}
+                    </span>
+                  </div>
                 </div>
+                <button
+                  className="absolute right-4 top-2.5 inline-flex h-6 w-6 items-center justify-center rounded-full bg-gray-200 text-slate-500 transition-colors duration-300 hover:bg-gray-300 hover:text-slate-600 active:bg-gray-400"
+                  onClick={() => onRemoveExpense(expense.id)}
+                >
+                  <X className="h-4 w-4 stroke-current" />
+                </button>
               </div>
             );
           })}
