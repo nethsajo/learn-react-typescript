ffc5cefebcb121e63f665179f29b8aa93e7d7dc5
Jan Kenneth Sajo <nethsajo98@gmail.com>
2024-01-23 15:37:41 +0800

    update usePopcorn

--- 3. Components, Composition, and Reusability/usePopcorn/src/App.tsx
+++ 3. Components, Composition, and Reusability/usePopcorn/src/App.tsx
@@ -1,3 +1,25 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { TMDB_API_KEY } from 'shared/constants/environments';
+
 export default function App() {
-  return <div>Hello, world!</div>;
+  const [page, setPage] = useState(1);
+  const [popularMovies, setPopularMovies] = useState({});
+
+  const handleSetPage = () => {};
+
+  useEffect(() => {
+    const handleFetchMovies = async () => {
+      try {
+        const { data } = await axios.get(
+          `https://api.themoviedb.org/3/movie/popular?language=en-US&page=${page}&api_key=${TMDB_API_KEY}`
+        );
+        setPopularMovies(data);
+      } catch (error) {}
+    };
+
+    handleFetchMovies();
+  }, [page]);
+
+  return <div>{JSON.stringify(popularMovies.results)}</div>;
 }
