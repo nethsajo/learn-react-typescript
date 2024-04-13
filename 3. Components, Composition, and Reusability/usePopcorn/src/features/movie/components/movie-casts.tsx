341708d6454331425409aa73af08e343a1c1343e
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2024-04-13 15:27:57 +0800

    add back button to movie page of usePopcorn

---
+++ 3. Components, Composition, and Reusability/usePopcorn/src/features/movie/components/movie-casts.tsx
@@ -0,0 +1,30 @@
+import { type MovieCast } from '../types';
+
+type Props = {
+  casts: MovieCast[];
+};
+
+export function MovieCasts({ casts }: Props) {
+  return (
+    <div className="space-y-4">
+      <h2 className="text-lg text-gray-300 md:text-xl">Casts</h2>
+      <div className="flex flex-nowrap items-start space-x-6 overflow-x-auto">
+        {casts.map(cast => {
+          return (
+            <div key={cast.id} className="flex flex-col">
+              <div className="relative grid h-full min-w-[9rem] max-w-[9rem] md:min-w-[10rem] md:max-w-[10rem]">
+                <img
+                  src={`https://image.tmdb.org/t/p/original${cast.profile_path}`}
+                  alt={`${cast.name} photo`}
+                  className="rounded-md"
+                />
+              </div>
+              <span className="mt-2 text-sm text-gray-200">{cast.name}</span>
+              <span className="text-gray-400">{cast.character}</span>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
--- 3. Components, Composition, and Reusability/usePopcorn/src/features/movie/components/movie.tsx
+++ 3. Components, Composition, and Reusability/usePopcorn/src/features/movie/components/movie.tsx
@@ -1,6 +1,8 @@
-import { Calendar, Clock } from 'lucide-react';
+import { Calendar, ChevronLeft, Clock } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
 
 import { type Movie, type MovieCast } from '../types';
+import { MovieCasts } from './movie-casts';
 import { MovieDescription } from './movie-description';
 import { MovieGenre } from './movie-genre';
 import { MovieTitle } from './movie-title';
@@ -12,6 +14,11 @@ type MovieProps = {
 
 export function Movie({ movie, casts }: MovieProps) {
   const directed = casts.find(cast => cast.known_for_department.toLowerCase() === 'directing');
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate(-1);
+  };
 
   return (
     <>
@@ -20,60 +27,43 @@ export function Movie({ movie, casts }: MovieProps) {
         className="fixed left-0 top-0 h-screen w-full object-cover"
       />
       <div className="fixed left-0 top-0 h-screen w-full bg-black/60 backdrop-blur-[2px]" />
-      <div className="relative">
-        <div className="mx-auto max-w-7xl p-6 md:p-16 lg:p-24">
-          <div className="mb-8 flex flex-col items-center justify-between md:flex-row md:items-start">
-            <div className="mb-4 space-y-5 md:mr-8">
-              <MovieTitle title={movie.title} average={movie.vote_average} />
-              <div className="flex items-center space-x-4 text-sm text-gray-200">
-                <div className="flex items-center space-x-1.5 text-sm">
-                  <Calendar size={20} className="text-gray-300" />
-                  <span>{movie.release_date}</span>
-                </div>
-                <div className="flex items-center space-x-1.5 text-sm">
-                  <Clock size={20} className="text-gray-300" />
-                  <span>
-                    {Math.floor(movie.runtime / 60)}:{movie.runtime % 60}m
-                  </span>
-                </div>
+      <div className="relative mx-auto max-w-7xl p-6 md:p-16 lg:p-24">
+        <button
+          className="flex h-9 w-9 items-center justify-center rounded-full bg-white text-gray-600 shadow-md transition-colors duration-500 hover:bg-gray-200 hover:text-gray-700 active:bg-gray-300 active:text-gray-700"
+          onClick={handleBack}
+        >
+          <ChevronLeft className="stroke-current" />
+        </button>
+        <div className="my-8 flex flex-col items-center justify-between md:flex-row md:items-start">
+          <div className="mb-4 space-y-5 md:mr-8">
+            <MovieTitle title={movie.title} average={movie.vote_average} />
+            <div className="flex items-center space-x-4 text-sm text-gray-200">
+              <div className="flex items-center space-x-1.5 text-sm">
+                <Calendar size={20} className="text-gray-300" />
+                <span>{movie.release_date}</span>
               </div>
-              <MovieDescription tagline={movie.tagline} overview={movie.overview} />
-              <div className="space-y-3">
-                <div className="flex items-center space-x-2">
-                  <span className="text-sm text-gray-300">Directed by:</span>
-                  <span className="text-sm font-medium text-gray-200">
-                    {directed?.name ?? 'N/A'}
-                  </span>
-                </div>
-                <div className="flex items-center space-x-2">
-                  <span className="text-sm text-gray-300">Genre:</span>
-                  <MovieGenre genres={movie.genres} />
-                </div>
+              <div className="flex items-center space-x-1.5 text-sm">
+                <Clock size={20} className="text-gray-300" />
+                <span>
+                  {Math.floor(movie.runtime / 60)}:{movie.runtime % 60}m
+                </span>
               </div>
             </div>
-            {/* <MoviePoster poster={movie.poster_path} /> */}
-          </div>
-          <div className="space-y-4">
-            <h2 className="text-lg text-gray-300 md:text-xl">Casts</h2>
-            <div className="flex flex-nowrap items-start space-x-6 overflow-x-auto">
-              {casts.map(cast => {
-                return (
-                  <div key={cast.id} className="flex flex-col">
-                    <div className="relative grid h-full min-w-[9rem] max-w-[9rem] md:min-w-[10rem] md:max-w-[10rem]">
-                      <img
-                        src={`https://image.tmdb.org/t/p/w500${cast.profile_path}`}
-                        alt={`${cast.name} photo`}
-                        className="rounded-md"
-                      />
-                    </div>
-                    <span className="mt-2 text-sm text-gray-200">{cast.name}</span>
-                    <span>{cast.character}</span>
-                  </div>
-                );
-              })}
+            <MovieDescription tagline={movie.tagline} overview={movie.overview} />
+            <div className="space-y-3">
+              <div className="flex items-center space-x-2">
+                <span className="text-sm text-gray-300">Directed by:</span>
+                <span className="text-sm font-medium text-gray-200">{directed?.name ?? 'N/A'}</span>
+              </div>
+              <div className="flex items-center space-x-2">
+                <span className="text-sm text-gray-300">Genre:</span>
+                <MovieGenre genres={movie.genres} />
+              </div>
             </div>
           </div>
+          {/* <MoviePoster poster={movie.poster_path} /> */}
         </div>
+        <MovieCasts casts={casts} />
       </div>
     </>
   );
