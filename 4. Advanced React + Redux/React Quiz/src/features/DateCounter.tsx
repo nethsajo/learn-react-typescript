b1d47106eb832f63de65027c50d9b6a14ce5cbb3
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2024-05-02 23:01:47 +0800

    update react quiz

--- 4. Advanced React + Redux/React Quiz/src/features/DateCounter.tsx
+++ 4. Advanced React + Redux/React Quiz/src/features/DateCounter.tsx
@@ -11,15 +11,25 @@ export default function DateCounter() {
     setStep(Number(e.target.value));
   };
 
+  const handleCount = (e: ChangeEvent<HTMLInputElement>) => {
+    setCount(Number(e.target.value));
+  };
+
   const handleIncrementCount = () => {
     setCount(prevCount => prevCount + step);
   };
+
   const handleDecrementCount = () => {
     setCount(prevCount => prevCount - step);
   };
 
+  const handleReset = () => {
+    setStep(1);
+    setCount(0);
+  };
+
   const classes =
-    'flex h-8 w-8 items-center justify-center rounded-full bg-blue-500 font-bold text-white';
+    'flex h-10 w-10 items-center justify-center rounded-md bg-blue-500 font-bold text-white';
 
   return (
     <div className="flex h-screen w-full items-center justify-center">
@@ -29,11 +39,16 @@ export default function DateCounter() {
             <input type="range" value={step} onChange={handleStep} min={0} max={10} />
             <span>{step}</span>
           </div>
-          <div className="flex items-center justify-center space-x-4">
+          <div className="flex items-center justify-center space-x-1">
             <button onClick={handleDecrementCount} className={classes}>
               -
             </button>
-            <input type="text" value={count} />
+            <input
+              type="text"
+              value={count}
+              onChange={handleCount}
+              className="flex h-10 w-full rounded-md border border-gray-300 px-3 py-2 text-sm disabled:cursor-not-allowed disabled:opacity-50"
+            />
             <button onClick={handleIncrementCount} className={classes}>
               +
             </button>
@@ -46,6 +61,15 @@ export default function DateCounter() {
                 }`}
             {date.toDateString()}
           </p>
+          <div className="flex justify-center">
+            <button
+              type="button"
+              onClick={handleReset}
+              className="rounded-sm bg-gray-200 px-3 py-1.5 text-sm font-medium text-gray-600 outline-none transition-colors duration-300 hover:bg-gray-300 focus:border-gray-500"
+            >
+              Reset
+            </button>
+          </div>
         </div>
       </div>
     </div>
