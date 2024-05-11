d55797bbcef8919a851e277b1f90dc88b68b44dd
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2024-05-11 15:01:52 +0800

    update react quiz

--- 4. Advanced React + Redux/React Quiz/src/App.tsx
+++ 4. Advanced React + Redux/React Quiz/src/App.tsx
@@ -1,16 +1,13 @@
-import ReactLogo from 'shared/assets/react.svg?react';
+import { QuizFooter } from './features/quiz/components/quiz-footer';
+import { QuizHeader } from './features/quiz/components/quiz-header';
+import { QuizList } from './features/quiz/components/quiz-list';
 
 export default function App() {
   return (
     <div className="mx-auto my-12 flex max-w-lg flex-col justify-center space-y-8 px-4 md:max-w-2xl">
-      <div className="flex items-center justify-center space-x-4 md:space-x-8">
-        <ReactLogo className="h-14 w-14 fill-sky-500 sm:h-20 sm:w-20 md:h-28 md:w-28" />
-        <h1 className="text-3xl font-extrabold uppercase tracking-wide text-slate-200 md:text-5xl">
-          The React Quiz
-        </h1>
-      </div>
-      <div className="">
-        <div className="relative mb-2 h-2 w-full overflow-hidden rounded-full bg-slate-700">
+      <QuizHeader />
+      <div className="flex flex-col space-y-4">
+        <div className="relative h-2 w-full overflow-hidden rounded-full bg-slate-700">
           <div
             className="absolute h-full bg-blue-500 transition-all duration-300"
             style={{ width: '15%' }}
@@ -26,29 +23,8 @@ export default function App() {
         </div>
       </div>
       <div className="flex flex-col space-y-6">
-        <p className="text-balance text-lg font-bold text-slate-300 sm:text-xl">
-          Which company invented React?
-        </p>
-        <div className="space-y-4">
-          <button className="bg- w-full rounded-full bg-slate-800 px-6 py-4 text-left font-medium text-slate-300 transition-all duration-300 hover:translate-x-4 hover:bg-transparent hover:ring-1 hover:ring-white/10">
-            Google
-          </button>
-          <button className="bg- w-full rounded-full bg-slate-800 px-6 py-4 text-left font-medium text-slate-300 transition-all duration-300 hover:translate-x-4 hover:bg-transparent hover:ring-1 hover:ring-white/10">
-            Apple
-          </button>
-          <button className="bg- w-full rounded-full bg-slate-800 px-6 py-4 text-left font-medium text-slate-300 transition-all duration-300 hover:translate-x-4 hover:bg-transparent hover:ring-1 hover:ring-white/10">
-            Netflix
-          </button>
-          <button className="bg- w-full rounded-full bg-slate-800 px-6 py-4 text-left font-medium text-slate-300 transition-all duration-300 hover:translate-x-4 hover:bg-transparent hover:ring-1 hover:ring-white/10">
-            Facebook
-          </button>
-        </div>
-        <div className="flex items-center justify-between">
-          <div className="rounded-md px-4 py-2 ring-1 ring-white/10">07:18</div>
-          <button className="rounded-sm bg-sky-500 px-6 py-1.5 font-medium text-slate-200 transition-colors duration-300 hover:bg-sky-600">
-            Next
-          </button>
-        </div>
+        <QuizList />
+        <QuizFooter />
       </div>
     </div>
   );
---
+++ 4. Advanced React + Redux/React Quiz/src/features/quiz/components/quiz-footer.tsx
@@ -0,0 +1,10 @@
+export function QuizFooter() {
+  return (
+    <div className="flex items-center justify-between">
+      <div className="rounded-md px-4 py-2 ring-1 ring-white/10">07:18</div>
+      <button className="rounded-sm bg-sky-500 px-6 py-1.5 font-medium text-slate-200 transition-colors duration-300 hover:bg-sky-600">
+        Next
+      </button>
+    </div>
+  );
+}
