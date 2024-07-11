0661122e367f71013e02c1486c5aa43569ee4f8e
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2024-07-11 23:01:22 +0800

    add responsiveness of worldwise navbar

--- 5. React Router - Building Single-Page Applications (SPA)/Worldwise/src/components/layout/page.tsx
+++ 5. React Router - Building Single-Page Applications (SPA)/Worldwise/src/components/layout/page.tsx
@@ -3,7 +3,7 @@ import { Navbar } from '../partials/navbar';
 
 export function PageLayout({ children }: React.PropsWithChildren) {
   return (
-    <main className="relative h-[calc(100vh-3rem)] px-12 py-6 lg:m-6">
+    <main className="relative h-[calc(100vh-3rem)] px-6 py-6 md:m-6 md:px-12">
       <div className="absolute inset-0 bg-[url('./bg.jpg')] bg-cover bg-center">
         <div className="h-full w-full bg-[rgba(36,42,46,0.8)]"></div>
       </div>
--- 5. React Router - Building Single-Page Applications (SPA)/Worldwise/src/components/partials/navbar.tsx
+++ 5. React Router - Building Single-Page Applications (SPA)/Worldwise/src/components/partials/navbar.tsx
@@ -11,17 +11,21 @@ export function Navbar() {
 
   return (
     <header>
-      <div className="flex items-center space-x-4">
-        <Link to="/" className="flex-1">
-          <img src="/logo.png" alt="Worldwise Logo" className="h-14" />
+      <div className="flex items-center justify-between">
+        <Link to="/">
+          <img src="/logo.png" alt="Worldwise Logo" className="h-10 sm:h-12 md:h-14" />
         </Link>
-        <div className="hidden space-x-8 font-medium uppercase text-slate-200">
-          <Link to="/pricing">Pricing</Link>
-          <Link to="/product">Product</Link>
-          <Link to="/login">Login</Link>
+        <div
+          className={`${isToggle ? 'pointer-events-auto visible translate-x-0 translate-y-0 opacity-100' : 'pointer-events-none invisible translate-x-full translate-y-full opacity-0'} fixed left-0 top-0 h-full w-full bg-black/40 backdrop-blur-sm transition-all duration-300 ease-in md:pointer-events-auto md:visible md:static md:w-auto md:translate-x-0 md:translate-y-0 md:bg-transparent md:opacity-100`}
+        >
+          <div className="flex h-full flex-col items-center justify-center space-y-8 font-medium uppercase text-slate-200 md:flex-row md:space-x-8 md:space-y-0">
+            <Link to="/pricing">Pricing</Link>
+            <Link to="/product">Product</Link>
+            <Link to="/login">Login</Link>
+          </div>
         </div>
-        <button onClick={handleToggleNav} className="text-slate-200">
-          {!isToggle ? <Menu /> : <X />}
+        <button onClick={handleToggleNav} className="z-10 flex text-slate-200 md:hidden">
+          {!isToggle ? <Menu className="h-8 w-8" /> : <X className="h-8 w-8" />}
         </button>
       </div>
     </header>
