f6ed5e3de8745f8a5042ba9323a359505b99adb5
Jan Kenneth Sajo <nethsajo98@gmail.com>
2024-07-12 13:17:21 +0800

    update worldwise about and pricing page

--- 5. React Router - Building Single-Page Applications (SPA)/Worldwise/src/routes/about.tsx
+++ 5. React Router - Building Single-Page Applications (SPA)/Worldwise/src/routes/about.tsx
@@ -3,21 +3,26 @@ import { PageLayout } from '@/components/layout/page';
 export default function AboutPage() {
   return (
     <PageLayout>
-      <section className="mx-auto grid max-w-4xl grid-cols-1 items-center justify-center gap-8 text-center sm:h-full sm:grid-cols-2">
-        <div className="mt-8 flex flex-col space-y-8 text-left sm:mt-0">
-          <h2 className="text-4xl font-medium text-slate-100 sm:text-balance">
-            Simple pricing. Just $9 month.
-          </h2>
-          <p className="text-base text-slate-300 sm:text-lg">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae vel labore mollitia
-            iusto. Recusandae quos provident, laboriosam fugit voluptatem iste.
-          </p>
-        </div>
+      <section className="mx-auto grid max-w-5xl grid-cols-1 items-center justify-center gap-16 pt-8 text-center sm:h-full sm:grid-cols-2">
         <img
-          src="/img-2.jpg"
-          alt="Overview of a large city with skyscrapers"
+          src="/img-1.jpg"
+          alt="A man with dog overlooking mountain with sunset"
           className="w-full rounded-md"
         />
+        <div className="flex flex-col space-y-8 text-left sm:mt-0">
+          <h2 className="text-4xl font-medium text-slate-100 sm:text-balance">About WorldWise.</h2>
+          <div className="space-y-6 text-balance text-base text-slate-300">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo est dicta illum vero
+              culpa cum quaerat architecto sapiente eius non soluta, molestiae nihil laborum,
+              placeat debitis, laboriosam at fuga perspiciatis?
+            </p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis doloribus libero
+              sunt expedita ratione iusto, magni, id sapiente sequi officiis et.
+            </p>
+          </div>
+        </div>
       </section>
     </PageLayout>
   );
