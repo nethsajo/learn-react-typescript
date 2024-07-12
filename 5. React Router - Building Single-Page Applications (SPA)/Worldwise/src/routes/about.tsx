41d82264d7a0683bac54922b6a7ad60dcf44dd3c
Jan Kenneth Sajo <nethsajo98@gmail.com>
2024-07-12 10:40:57 +0800

    add worldwise about page

--- 5. React Router - Building Single-Page Applications (SPA)/Worldwise/src/routes/about.tsx
+++ 5. React Router - Building Single-Page Applications (SPA)/Worldwise/src/routes/about.tsx
@@ -3,7 +3,22 @@ import { PageLayout } from '@/components/layout/page';
 export default function AboutPage() {
   return (
     <PageLayout>
-      <h1>Hello from About Page!</h1>
+      <section className="mx-auto grid max-w-4xl grid-cols-1 items-center justify-center gap-8 text-center sm:h-full sm:grid-cols-2">
+        <div className="mt-8 flex flex-col space-y-8 text-left sm:mt-0">
+          <h2 className="text-4xl font-medium text-slate-100 sm:text-balance">
+            Simple pricing. Just $9 month.
+          </h2>
+          <p className="text-base text-slate-300 sm:text-lg">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae vel labore mollitia
+            iusto. Recusandae quos provident, laboriosam fugit voluptatem iste.
+          </p>
+        </div>
+        <img
+          src="/img-2.jpg"
+          alt="Overview of a large city with skyscrapers"
+          className="w-full rounded-md"
+        />
+      </section>
     </PageLayout>
   );
 }
