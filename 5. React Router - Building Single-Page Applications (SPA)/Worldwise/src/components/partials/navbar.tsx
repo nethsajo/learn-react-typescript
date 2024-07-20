f343f8f15e3dcd0bc3e4ab6339f3c1842638c5fe
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2024-07-20 14:49:40 +0800

    update worldwise navbar

--- 5. React Router - Building Single-Page Applications (SPA)/Worldwise/src/components/partials/navbar.tsx
+++ 5. React Router - Building Single-Page Applications (SPA)/Worldwise/src/components/partials/navbar.tsx
@@ -1,6 +1,6 @@
 import { Menu, X } from 'lucide-react';
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 export function Navbar() {
   const [isToggle, setIsToggle] = useState(false);
@@ -16,11 +16,21 @@ export function Navbar() {
           <img src="/logo.png" alt="Worldwise Logo" className="h-10 sm:h-12" />
         </Link>
         <div
-          className={`${isToggle ? 'pointer-events-auto visible translate-x-0 translate-y-0 opacity-100' : 'pointer-events-none invisible translate-x-full translate-y-full opacity-0'} fixed left-0 top-0 h-full w-full bg-black/40 backdrop-blur-sm transition-all duration-300 ease-in md:pointer-events-auto md:visible md:static md:w-auto md:translate-x-0 md:translate-y-0 md:bg-transparent md:opacity-100`}
+          className={`${isToggle ? 'pointer-events-auto visible translate-x-0 translate-y-0 opacity-100' : 'pointer-events-none invisible translate-x-full translate-y-full opacity-0'} fixed left-0 top-0 h-full w-full bg-gray-600 backdrop-blur-sm transition-all duration-300 ease-in md:pointer-events-auto md:visible md:static md:w-auto md:translate-x-0 md:translate-y-0 md:bg-transparent md:opacity-100`}
         >
           <div className="flex h-full flex-col items-center justify-center space-y-10 text-base font-medium uppercase text-slate-200 md:flex-row md:space-x-10 md:space-y-0">
-            <Link to="/pricing">Pricing</Link>
-            <Link to="/product">About</Link>
+            <NavLink
+              to="/pricing"
+              className={({ isActive }) => (isActive ? 'text-emerald-600' : 'text-inherit')}
+            >
+              Pricing
+            </NavLink>
+            <NavLink
+              to="/product"
+              className={({ isActive }) => (isActive ? 'text-emerald-600' : 'text-inherit')}
+            >
+              About
+            </NavLink>
             <Link to="/login" className="rounded-sm bg-emerald-500 px-4 py-1.5 text-slate-200">
               Login
             </Link>
