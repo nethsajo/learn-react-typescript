bde2a3c8dee0700c5f7d4e00bba013cdb645d616
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2024-08-22 12:33:26 +0800

    update worldwise sidebar component

--- 5. React Router - Building Single-Page Applications (SPA)/Worldwise/src/features/app/sidebar.tsx
+++ 5. React Router - Building Single-Page Applications (SPA)/Worldwise/src/features/app/sidebar.tsx
@@ -1,6 +1,8 @@
-import { Link, NavLink, Outlet } from 'react-router-dom';
+import { Link, NavLink, Outlet, useLocation } from 'react-router-dom';
 
 export function Sidebar() {
+  const location = useLocation();
+
   return (
     <div className="h-full w-[560px] rounded-bl-md rounded-tl-md bg-gray-700 p-6">
       <div className="flex h-full flex-col items-center">
@@ -12,7 +14,7 @@ export function Sidebar() {
             <NavLink
               to="cities"
               className={({ isActive }) =>
-                `inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1 text-sm font-medium ring-offset-gray-800 transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-emerald-500 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 ${isActive ? 'bg-gray-600 text-gray-200 shadow-sm' : 'bg-transparent text-gray-400'}`
+                `inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1 text-sm font-medium ring-offset-gray-800 transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-emerald-500 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 ${isActive || location.pathname === '/app' ? 'bg-gray-600 text-gray-200 shadow-sm' : 'bg-transparent text-gray-400'}`
               }
             >
               Cities
