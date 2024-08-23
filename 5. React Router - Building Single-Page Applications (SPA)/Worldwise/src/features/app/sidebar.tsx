7c38333d0a48e7218132c1f21a43a67077913f93
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2024-08-23 12:43:28 +0800

    update worldwise sidebar

--- 5. React Router - Building Single-Page Applications (SPA)/Worldwise/src/features/app/sidebar.tsx
+++ 5. React Router - Building Single-Page Applications (SPA)/Worldwise/src/features/app/sidebar.tsx
@@ -1,3 +1,4 @@
+import { ROUTES } from '@/constants/routes';
 import { Link, NavLink, Outlet, useLocation } from 'react-router-dom';
 
 export function Sidebar() {
@@ -5,14 +6,14 @@ export function Sidebar() {
 
   return (
     <div className="h-full w-[560px] rounded-bl-md rounded-tl-md bg-gray-700 p-6">
-      <div className="flex h-full flex-col items-center">
-        <div className="flex flex-col items-center space-y-8">
+      <div className="flex h-full flex-col items-center overflow-hidden">
+        <div className="flex w-full flex-col items-center space-y-8 px-10">
           <Link to="/">
             <img src="/logo.png" alt="Worldwise Logo" className="h-10 sm:h-12" />
           </Link>
           <div className="flex items-center rounded-md bg-gray-800 p-1" tabIndex={0}>
             <NavLink
-              to="cities"
+              to={ROUTES.CITIES}
               className={({ isActive }) =>
                 `inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1 text-sm font-medium ring-offset-gray-800 transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-emerald-500 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 ${isActive || location.pathname === '/app' ? 'bg-gray-600 text-gray-200 shadow-sm' : 'bg-transparent text-gray-400'}`
               }
@@ -20,7 +21,7 @@ export function Sidebar() {
               Cities
             </NavLink>
             <NavLink
-              to="countries"
+              to={ROUTES.COUNTRIES}
               className={({ isActive }) =>
                 `inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1 text-sm font-medium ring-offset-gray-800 transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-emerald-500 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 ${isActive ? 'bg-gray-600 text-gray-200 shadow-sm' : 'bg-transparent text-gray-400'}`
               }
