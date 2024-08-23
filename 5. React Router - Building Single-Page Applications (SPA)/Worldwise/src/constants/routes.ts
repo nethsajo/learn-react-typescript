93e53936fa60f0a06131666b76f20e392d4d5296
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2024-08-23 11:32:50 +0800

    add constant routes to worldwise

---
+++ 5. React Router - Building Single-Page Applications (SPA)/Worldwise/src/constants/routes.ts
@@ -0,0 +1,9 @@
+export const ROUTES = {
+  HOME: '/',
+  PRODUCT: '/product',
+  PRICING: '/pricing',
+  LOGIN: '/login',
+  APP: '/app',
+  CITIES: 'cities',
+  COUNTRIES: 'countries',
+} as const;
