ac72aba6fffd9354fa9d1555c7d815c02480a8b0
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2024-08-29 22:35:37 +0800

    update form city component

--- 5. React Router - Building Single-Page Applications (SPA)/Worldwise/src/features/app/cities/components/add-city-form.tsx
+++ 5. React Router - Building Single-Page Applications (SPA)/Worldwise/src/features/app/cities/components/add-city-form.tsx
@@ -1,4 +1,6 @@
 import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import { ChevronLeft } from 'lucide-react';
 import { type FormEvent } from 'react';
 
@@ -8,28 +10,14 @@ export function AddCityForm() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="w-full space-y-4 rounded-md bg-gray-600 p-4">
+    <form onSubmit={handleSubmit} className="w-full space-y-4 rounded-lg bg-gray-600 p-4">
       <div className="flex flex-col space-y-1.5">
-        <label htmlFor="city" className="text-sm text-gray-100">
-          City Name
-        </label>
-        <input
-          type="text"
-          id="city"
-          readOnly
-          className="block w-full rounded-md border-0 bg-gray-200 bg-transparent py-1.5 text-gray-300 shadow-sm ring-1 ring-inset ring-gray-400 read-only:bg-gray-300 read-only:ring-0 focus:ring-2 focus:ring-inset focus:ring-emerald-600 read-only:focus:ring-0 sm:text-sm sm:leading-6"
-        />
+        <Label htmlFor="city">City Name</Label>
+        <Input variant="outline" id="city" />
       </div>
       <div className="flex flex-col space-y-1.5">
-        <label htmlFor="date" className="text-sm text-gray-100">
-          When did you go to X?
-        </label>
-        <input
-          type="date"
-          name="date"
-          id="date"
-          className="block w-full rounded-md border-0 bg-gray-200 bg-transparent py-1.5 text-gray-300 shadow-sm ring-1 ring-inset ring-gray-400 focus:ring-2 focus:ring-inset focus:ring-emerald-600 sm:text-sm sm:leading-6"
-        />
+        <Label htmlFor="date">When did you go to X?</Label>
+        <Input type="date" variant="outline" id="date" />
       </div>
       <div className="flex flex-col space-y-1.5">
         <label htmlFor="notes" className="text-sm text-gray-100">
