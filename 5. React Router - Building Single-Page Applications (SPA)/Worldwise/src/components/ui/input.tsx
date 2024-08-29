1907e43dc79bddd448548fcef0f5345cdcf9ffc8
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2024-08-29 22:33:53 +0800

    update input component

--- 5. React Router - Building Single-Page Applications (SPA)/Worldwise/src/components/ui/input.tsx
+++ 5. React Router - Building Single-Page Applications (SPA)/Worldwise/src/components/ui/input.tsx
@@ -4,23 +4,25 @@ import { cva, type VariantProps } from 'class-variance-authority';
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement>,
     VariantProps<typeof inputVariants> {
-  children: React.ReactNode;
+  label?: string;
 }
 
-const inputVariants = cva('block w-full', {
-  variants: {
-    variant: {
-      filled: '',
-      outline: '',
-      underlined: '',
+const inputVariants = cva(
+  'block w-full rounded-md h-10 px-3 py-2 shadow-sm read-only:focus:ring-0 read-only:ring-0 sm:text-sm sm:leading-6 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-200 focus-visible:ring-offset-2 focus-visible:ring-offset-gray-600 disabled:cursor-not-allowed disabled:opacity-50',
+  {
+    variants: {
+      variant: {
+        filled: 'bg-gray-200 read-only:bg-gray-300 ring-gray-400',
+        outline: 'border border-zinc-400/60 bg-transparent text-gray-200 dark:[color-scheme:dark]',
+      },
     },
-  },
-  defaultVariants: {
-    variant: 'filled',
-  },
-});
+    defaultVariants: {
+      variant: 'filled',
+    },
+  }
+);
 
-const Input = ({ className, variant, type = 'text', ...props }: InputProps) => {
+const Input = ({ type = 'text', className, variant, ...props }: InputProps) => {
   return <input type={type} {...props} className={cn(inputVariants({ variant, className }))} />;
 };
 
