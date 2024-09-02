6b4d09da77ce98c41d36b326473a238f54de2f64
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2024-09-02 22:01:44 +0800

    add back function

--- 5. React Router - Building Single-Page Applications (SPA)/Worldwise/src/features/app/cities/components/add-city-form.tsx
+++ 5. React Router - Building Single-Page Applications (SPA)/Worldwise/src/features/app/cities/components/add-city-form.tsx
@@ -3,10 +3,18 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
 import { type FormEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export function AddCityForm() {
+  const navigate = useNavigate();
+
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
+  };
+
+  const handleBack = (e: FormEvent) => {
+    e.preventDefault();
+    navigate(-1);
   };
 
   return (
@@ -24,7 +32,9 @@ export function AddCityForm() {
         <Textarea rows={3} variant="outline" id="notes" />
       </div>
       <div className="flex items-center justify-between">
-        <Button variant="secondary">Back</Button>
+        <Button variant="secondary" onClick={handleBack}>
+          Back
+        </Button>
         <Button type="submit">Submit</Button>
       </div>
     </form>
