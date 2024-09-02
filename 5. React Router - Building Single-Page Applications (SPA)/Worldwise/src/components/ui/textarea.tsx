dbe7f8aeb382ae1ca6c45f67e7f3a380f4a81a84
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2024-09-02 20:42:35 +0800

    update textarea component

--- 5. React Router - Building Single-Page Applications (SPA)/Worldwise/src/components/ui/textarea.tsx
+++ 5. React Router - Building Single-Page Applications (SPA)/Worldwise/src/components/ui/textarea.tsx
@@ -1,1 +1,29 @@
-export const Textarea = () => {};
+import { cn } from '@/utils/classnames';
+import { cva, type VariantProps } from 'class-variance-authority';
+
+export interface TextareaProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement>,
+    VariantProps<typeof textareaVariants> {
+  label?: string;
+}
+
+const textareaVariants = cva(
+  'block w-full rounded-md resize-none px-3 py-2 shadow-sm read-only:focus:ring-0 read-only:ring-0 sm:text-sm sm:leading-6 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-200 focus-visible:ring-offset-2 focus-visible:ring-offset-gray-600 disabled:cursor-not-allowed disabled:opacity-50',
+  {
+    variants: {
+      variant: {
+        filled: 'bg-gray-200 read-only:bg-gray-300 ring-gray-400',
+        outline: 'border border-zinc-400/60 bg-transparent text-gray-200',
+      },
+    },
+    defaultVariants: {
+      variant: 'filled',
+    },
+  }
+);
+
+export const Textarea = ({ rows = 5, className, variant, ...props }: TextareaProps) => {
+  return (
+    <textarea rows={rows} {...props} className={cn(textareaVariants({ variant, className }))} />
+  );
+};
