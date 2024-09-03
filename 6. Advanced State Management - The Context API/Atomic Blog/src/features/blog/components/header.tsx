740e8b2817bba2e87670e9daa8f62bfa8838a2c5
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2024-09-03 21:53:31 +0800

    add atomic blog header component

---
+++ 6. Advanced State Management - The Context API/Atomic Blog/src/features/blog/components/header.tsx
@@ -0,0 +1,21 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Atom } from 'lucide-react';
+
+export function Header() {
+  return (
+    <header className="grid w-full grid-cols-[1fr_auto] items-center gap-x-2 gap-y-4 md:grid-cols-[1fr_auto_auto_auto]">
+      <div className="inline-flex items-center space-x-1 text-gray-900">
+        <Atom size={32} className="stroke-indigo-900" />
+        <h1 className="text-xl font-bold sm:text-2xl">The Atomic Blog</h1>
+      </div>
+      <p className="w-full font-medium text-gray-500">
+        🚀 <span className="text-sm font-bold text-gray-900">X</span> atomic posts found
+      </p>
+      <Input id="search" scale="lg" variant="outline" placeholder="Search posts..." />
+      <Button size="lg" variant="default">
+        Clear posts
+      </Button>
+    </header>
+  );
+}
