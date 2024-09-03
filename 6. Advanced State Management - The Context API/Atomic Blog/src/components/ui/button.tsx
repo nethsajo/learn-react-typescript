994b1b66b89b656b5a8232c4568d24bfce350046
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2024-09-03 21:53:00 +0800

    update atomic blog button component

--- 6. Advanced State Management - The Context API/Atomic Blog/src/components/ui/button.tsx
+++ 6. Advanced State Management - The Context API/Atomic Blog/src/components/ui/button.tsx
@@ -8,11 +8,11 @@ export interface ButtonProps
 }
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-black focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-white/70 disabled:pointer-events-none disabled:opacity-50 transition-colors',
+  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-900 focus-visible:ring-offset-2 focus-visible:ring-offset-white disabled:cursor-not-allowed disabled:pointer-events-none disabled:opacity-50 transition-colors',
   {
     variants: {
       variant: {
-        default: 'bg-emerald-500 text-white hover:bg-emerald-600 active:bg-emerald-700',
+        default: 'bg-indigo-500 text-white hover:bg-indigo-600 active:bg-indigo-700',
         destructive: 'bg-red-500 text-white hover:bg-red-600 active:bg-red-700',
         outline: 'border border-gray-500 hover:bg-gray-500 hover:text-white',
         secondary: 'bg-gray-500 hover:bg-gray-500/70 hover:text-white',
@@ -20,16 +20,14 @@ const buttonVariants = cva(
         link: 'text-white hover:underline underline-offset-4',
       },
       size: {
-        default: 'px-2 py-1',
-        sm: 'px-2 py-1',
-        md: 'px-2.5 py-1.5',
-        lg: 'px-3 py-2',
-        xl: 'px-3.5 py-2.5',
+        sm: 'px-2 py-1 h-8',
+        lg: 'px-3 py-2 h-9',
+        xl: 'px-3.5 py-2.5 h-11',
       },
     },
     defaultVariants: {
       variant: 'default',
-      size: 'default',
+      size: 'sm',
     },
   }
 );
