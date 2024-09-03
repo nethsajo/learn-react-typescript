ed9ad94996763d18a6a470416f5d12fc204be9cd
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2024-09-03 22:12:49 +0800

    add fakerjs to atomic blog

--- 6. Advanced State Management - The Context API/Atomic Blog/tailwind.config.js
+++ 6. Advanced State Management - The Context API/Atomic Blog/tailwind.config.js
@@ -3,7 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: [
-    './src/features/**/*.{ts,tsx}',
+    './src/features/**/**/*.{ts,tsx}',
     './src/components/**/*.{ts,tsx}',
     './src/routes/**/*.{ts,tsx}',
   ],
