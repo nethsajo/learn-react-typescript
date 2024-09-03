2bba55a1bcc90cc136f19e1d809ae5a9be8e5d1e
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2024-09-03 21:51:01 +0800

    update atomic blog app

--- 6. Advanced State Management - The Context API/Atomic Blog/src/App.tsx
+++ 6. Advanced State Management - The Context API/Atomic Blog/src/App.tsx
@@ -1,16 +1,13 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import React from 'react';
 import { RootLayout } from './components/layout/root';
-
-const queryClient = new QueryClient();
+import AtomicBlog from './features/blog';
 
 export default function App() {
   return (
-    <QueryClientProvider client={queryClient}>
+    <React.Fragment>
       <RootLayout>
-        <h1>Hello, world!</h1>
+        <AtomicBlog />
       </RootLayout>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    </React.Fragment>
   );
 }
