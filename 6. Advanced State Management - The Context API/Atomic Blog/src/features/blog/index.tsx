9acc95e8b7234574eabae85dc1b9506574327362
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2024-09-04 22:00:24 +0800

    update atomic blog index page

--- 6. Advanced State Management - The Context API/Atomic Blog/src/features/blog/index.tsx
+++ 6. Advanced State Management - The Context API/Atomic Blog/src/features/blog/index.tsx
@@ -1,11 +1,24 @@
-import { AddBlog } from './components/add-blog';
+import { type Blog } from '@/types/blog';
+import { useState } from 'react';
+import { AddBlogForm } from './components/add-blog-form';
+import { BlogList } from './components/blog-list';
 import { Header } from './components/header';
+import { createRandomPost } from './utils/create-random-post';
 
 export default function AtomicBlog() {
+  const [blogs, setBlogs] = useState<Blog[]>(() =>
+    Array.from({ length: 30 }, () => createRandomPost())
+  );
+
+  const handleSubmitBlog = (blog: Blog) => {
+    setBlogs(blogs => [...blogs, blog]);
+  };
+
   return (
     <div className="mx-auto flex max-w-7xl flex-col space-y-8 px-4 py-12 sm:px-6">
-      <Header />
-      <AddBlog />
+      <Header blogCount={blogs.length} />
+      <AddBlogForm onSubmitBlog={handleSubmitBlog} />
+      <BlogList blogs={blogs} />
     </div>
   );
 }
