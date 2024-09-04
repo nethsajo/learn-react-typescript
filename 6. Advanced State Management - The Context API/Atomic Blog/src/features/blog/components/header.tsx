d37a460742c2493aa658407fcf7f074daf67b008
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2024-09-04 22:16:01 +0800

    add search and clear blog functionalities

--- 6. Advanced State Management - The Context API/Atomic Blog/src/features/blog/components/header.tsx
+++ 6. Advanced State Management - The Context API/Atomic Blog/src/features/blog/components/header.tsx
@@ -1,12 +1,20 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Atom } from 'lucide-react';
+import { type ChangeEvent } from 'react';
 
 export type HeaderProps = {
   blogCount: number;
+  query: string;
+  setQuery: (value: string) => void;
+  onClearBlog: () => void;
 };
 
-export function Header({ blogCount }: HeaderProps) {
+export function Header({ blogCount, query, setQuery, onClearBlog }: HeaderProps) {
+  const handleChangeQuery = (event: ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
+
   return (
     <header className="grid w-full grid-cols-[1fr_auto] items-center gap-x-4 gap-y-4 md:grid-cols-[1fr_auto_auto_auto]">
       <div className="inline-flex items-center space-x-1 text-gray-900">
@@ -16,8 +24,15 @@ export function Header({ blogCount }: HeaderProps) {
       <p className="w-full font-medium text-gray-500">
         🚀 <span className="text-sm font-bold text-gray-900">{blogCount}</span> atomic posts found
       </p>
-      <Input id="search" scale="lg" variant="outline" placeholder="Search posts..." />
-      <Button size="lg" variant="default">
+      <Input
+        id="search"
+        scale="lg"
+        variant="outline"
+        placeholder="Search posts..."
+        value={query}
+        onChange={handleChangeQuery}
+      />
+      <Button size="lg" variant="default" onClick={onClearBlog}>
         Clear posts
       </Button>
     </header>
--- 6. Advanced State Management - The Context API/Atomic Blog/src/features/blog/index.tsx
+++ 6. Advanced State Management - The Context API/Atomic Blog/src/features/blog/index.tsx
@@ -6,6 +6,7 @@ import { Header } from './components/header';
 import { createRandomPost } from './utils/create-random-post';
 
 export default function AtomicBlog() {
+  const [query, setQuery] = useState('');
   const [blogs, setBlogs] = useState<Blog[]>(() =>
     Array.from({ length: 30 }, () => createRandomPost())
   );
@@ -14,11 +15,25 @@ export default function AtomicBlog() {
     setBlogs(blogs => [...blogs, blog]);
   };
 
+  const handleClearBlog = () => {
+    setBlogs([]);
+  };
+
+  const posts =
+    query.length > 0
+      ? blogs.filter(blog => `${blog.title} ${blog.body}`.toLowerCase().includes(query))
+      : blogs;
+
   return (
     <div className="mx-auto flex max-w-7xl flex-col space-y-8 px-4 py-12 sm:px-6">
-      <Header blogCount={blogs.length} />
+      <Header
+        query={query}
+        setQuery={setQuery}
+        onClearBlog={handleClearBlog}
+        blogCount={posts.length}
+      />
       <AddBlogForm onSubmitBlog={handleSubmitBlog} />
-      <BlogList blogs={blogs} />
+      <BlogList blogs={posts} />
     </div>
   );
 }
