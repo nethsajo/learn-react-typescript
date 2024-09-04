b33e0574ec89fa274f1fb274cb802d7a9ff0bde8
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2024-09-04 21:59:28 +0800

    add title and body input event

--- 6. Advanced State Management - The Context API/Atomic Blog/src/features/blog/components/add-blog-form.tsx
+++ 6. Advanced State Management - The Context API/Atomic Blog/src/features/blog/components/add-blog-form.tsx
@@ -1,18 +1,52 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
-import { type FormEvent } from 'react';
+import { type Blog } from '@/types/blog';
+import { useState, type ChangeEvent, type FormEvent } from 'react';
 
-export function AddBlogForm() {
+export type AddBlogFormProps = {
+  onSubmitBlog: (blog: Blog) => void;
+};
+
+export function AddBlogForm({ onSubmitBlog }: AddBlogFormProps) {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
+  const handleChangeTitle = (event: ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  };
+
+  const handleChangeBody = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setBody(event.target.value);
+  };
+
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
+    onSubmitBlog({ title, body });
+
+    setTitle('');
+    setBody('');
   };
 
   return (
     <div className="w-full rounded-md bg-gray-100 p-4">
       <form onSubmit={handleSubmit} className="space-y-4">
-        <Input id="title" variant="outline" scale="sm" placeholder="Post title" />
-        <Textarea rows={3} variant="outline" placeholder="Post body" />
+        <Input
+          id="title"
+          variant="outline"
+          scale="sm"
+          placeholder="Post title"
+          value={title}
+          onChange={handleChangeTitle}
+        />
+        <Textarea
+          id="body"
+          rows={3}
+          variant="outline"
+          placeholder="Post body"
+          value={body}
+          onChange={handleChangeBody}
+        />
         <div className="flex justify-end">
           <Button type="submit">Add post</Button>
         </div>
