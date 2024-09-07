79b7a3a2778844c16a07b5155b02280cc1675b88
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2024-09-07 22:39:11 +0800

    add post context and provider for atomic blog

---
+++ 6. Advanced State Management - The Context API/Atomic Blog/src/features/blog/contexts/post.tsx
@@ -0,0 +1,75 @@
+import { createRandomPost } from '@/features/blog/utils/create-random-post';
+import { type Blog } from '@/types/blog';
+import React, { useState } from 'react';
+
+// 1. CREATE CONTEXT
+
+type Children = {
+  children: React.ReactNode;
+};
+
+type State = {
+  posts: Blog[];
+  query: string;
+  isToggle: boolean;
+  onToggleForm: () => void;
+  onSetQuery: (q: string) => void;
+  onClearBlog: () => void;
+  onSubmitBlog: (blog: Blog) => void;
+};
+
+const PostContext = React.createContext<State>({
+  posts: [],
+  query: '',
+  isToggle: false,
+  onToggleForm: () => {},
+  onSetQuery: () => {},
+  onClearBlog: () => {},
+  onSubmitBlog: () => {},
+});
+
+const PostProvider = ({ children }: Children) => {
+  const [query, setQuery] = useState('');
+  const [isToggle, setIsToggle] = useState(false);
+  //The callback function will only run in initial render
+  const [blogs, setBlogs] = useState<Blog[]>(() =>
+    Array.from({ length: 30 }, () => createRandomPost())
+  );
+
+  const handleSubmitBlog = (blog: Blog) => {
+    setBlogs(blogs => [...blogs, blog]);
+  };
+
+  const handleSetQuery = (q: string) => {
+    setQuery(q);
+  };
+
+  const handleClearBlog = () => setBlogs([]);
+
+  const handleToggleForm = () => setIsToggle(toggle => !toggle);
+
+  // Derived state. These are the posts that will actually be displayed
+  const filteredPosts = blogs.filter(blog =>
+    `${blog.title} ${blog.body}`.toLowerCase().includes(query)
+  );
+
+  const posts = query.length > 0 ? filteredPosts : blogs;
+
+  return (
+    <PostContext.Provider
+      value={{
+        posts,
+        query,
+        isToggle,
+        onToggleForm: handleToggleForm,
+        onSetQuery: handleSetQuery,
+        onClearBlog: handleClearBlog,
+        onSubmitBlog: handleSubmitBlog,
+      }}
+    >
+      {children}
+    </PostContext.Provider>
+  );
+};
+
+export { PostContext, PostProvider };
