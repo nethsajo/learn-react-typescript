6790ff5503c27c45928983d4fdca27deddd8be4e
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2024-09-07 23:04:49 +0800

    update post context

--- 6. Advanced State Management - The Context API/Atomic Blog/src/features/blog/contexts/post.tsx
+++ 6. Advanced State Management - The Context API/Atomic Blog/src/features/blog/contexts/post.tsx
@@ -74,6 +74,7 @@ const PostProvider = ({ children }: Children) => {
 
 const usePosts = () => {
   const context = useContext(PostContext);
+  if (context === undefined) throw new Error('PostContext was used outside of the PostProvider');
   return context;
 };
 
