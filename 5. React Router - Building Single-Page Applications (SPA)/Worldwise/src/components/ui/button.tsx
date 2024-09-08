26a3bb4f4d6a5f61f800bb507bba136e754de924
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2024-09-08 13:13:59 +0800

    update button and input components

--- 5. React Router - Building Single-Page Applications (SPA)/Worldwise/src/components/ui/button.tsx
+++ 5. React Router - Building Single-Page Applications (SPA)/Worldwise/src/components/ui/button.tsx
@@ -8,7 +8,7 @@ export interface ButtonProps
 }
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-black focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-white/70 disabled:pointer-events-none disabled:opacity-50 transition-colors',
+  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-200 focus-visible:ring-offset-2 focus-visible:ring-offset-gray-600 disabled:cursor-not-allowed disabled:pointer-events-none disabled:opacity-50 transition-colors',
   {
     variants: {
       variant: {
