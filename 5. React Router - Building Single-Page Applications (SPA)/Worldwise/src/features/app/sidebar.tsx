f8768c78390ea23e727b23563b20787df1ff8f15
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2024-09-12 23:24:17 +0800

    fix sidebar height

--- 5. React Router - Building Single-Page Applications (SPA)/Worldwise/src/features/app/sidebar.tsx
+++ 5. React Router - Building Single-Page Applications (SPA)/Worldwise/src/features/app/sidebar.tsx
@@ -4,11 +4,11 @@ import { SidebarLink } from './sidebar-link';
 
 export function Sidebar() {
   return (
-    <div className="order-2 h-2/3 w-full bg-gray-700 p-4 lg:order-1 lg:h-full lg:w-[560px] lg:rounded-bl-md lg:rounded-tl-md lg:p-6">
+    <div className="order-2 h-3/5 w-full bg-gray-700 p-4 md:h-2/3 lg:order-1 lg:h-full lg:w-[560px] lg:rounded-bl-md lg:rounded-tl-md lg:p-6">
       <div className="flex h-full flex-col items-center overflow-hidden">
         <div className="flex w-full flex-col items-center space-y-8 lg:px-10">
           <Link to="/">
-            <img src="/logo.png" alt="Worldwise Logo" className="h-10 sm:h-12" />
+            <img src="/logo.png" alt="Worldwise Logo" className="hidden h-10 sm:h-12 md:block" />
           </Link>
           <div className="flex items-center rounded-md bg-gray-800 p-1 outline-none" tabIndex={0}>
             <SidebarLink to={ROUTES.CITIES}>Cities</SidebarLink>
