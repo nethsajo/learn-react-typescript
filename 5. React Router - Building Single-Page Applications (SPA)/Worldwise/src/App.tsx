dcc81a0a8eaf72fc1b86c365a70a9d8c7153970b
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2024-09-12 22:36:10 +0800

    update worldwise app

--- 5. React Router - Building Single-Page Applications (SPA)/Worldwise/src/App.tsx
+++ 5. React Router - Building Single-Page Applications (SPA)/Worldwise/src/App.tsx
@@ -4,6 +4,7 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import { RootLayout } from './components/layout/root';
 import { ROUTES } from './constants/routes';
 import { AddCityForm } from './features/app/cities/components/add-city-form';
+import { CityProvider } from './features/app/cities/contexts/CityContext';
 import HomePage from './routes';
 import AboutPage from './routes/about';
 import AppPage from './routes/app';
@@ -19,26 +20,28 @@ const queryClient = new QueryClient();
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <RootLayout>
-        <Routes>
-          <Route path={ROUTES.HOME} element={<HomePage />}></Route>
-          <Route path={ROUTES.PRODUCT} element={<AboutPage />}></Route>
-          <Route path={ROUTES.PRICING} element={<PricingPage />}></Route>
-          <Route path={ROUTES.LOGIN} element={<LoginPage />}></Route>
-          <Route path={ROUTES.APP} element={<AppPage />}>
-            {/* Immediately navigate to the /cities using the Navigate component  */}
-            {/* In order to fix the back, we need to add the `replace` keyword  */}
-            {/* Replace - it will replace the current element in the history stack */}
-            <Route index element={<Navigate replace to={ROUTES.CITIES} />}></Route>
-            <Route path={ROUTES.CITIES} element={<CitiesPage />}></Route>
-            <Route path={`${ROUTES.CITIES}/:id`} element={<CityPage />}></Route>
-            <Route path={ROUTES.COUNTRIES} element={<CountriesPage />}></Route>
-            <Route path={ROUTES.FORM} element={<AddCityForm />}></Route>
-          </Route>
-          {/* Will be matched if none of the other routes are matched */}
-          <Route path="*" element={<NotFoundPage />}></Route>
-        </Routes>
-      </RootLayout>
+      <CityProvider>
+        <RootLayout>
+          <Routes>
+            <Route path={ROUTES.HOME} element={<HomePage />}></Route>
+            <Route path={ROUTES.PRODUCT} element={<AboutPage />}></Route>
+            <Route path={ROUTES.PRICING} element={<PricingPage />}></Route>
+            <Route path={ROUTES.LOGIN} element={<LoginPage />}></Route>
+            <Route path={ROUTES.APP} element={<AppPage />}>
+              {/* Immediately navigate to the /cities using the Navigate component  */}
+              {/* In order to fix the back, we need to add the `replace` keyword  */}
+              {/* Replace - it will replace the current element in the history stack */}
+              <Route index element={<Navigate replace to={ROUTES.CITIES} />}></Route>
+              <Route path={ROUTES.CITIES} element={<CitiesPage />}></Route>
+              <Route path={`${ROUTES.CITIES}/:id`} element={<CityPage />}></Route>
+              <Route path={ROUTES.COUNTRIES} element={<CountriesPage />}></Route>
+              <Route path={ROUTES.FORM} element={<AddCityForm />}></Route>
+            </Route>
+            {/* Will be matched if none of the other routes are matched */}
+            <Route path="*" element={<NotFoundPage />}></Route>
+          </Routes>
+        </RootLayout>
+      </CityProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
