7783c5fdf887a6a26988ba1f3b898ec6b431b436
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2024-09-12 22:35:44 +0800

    add city item border when active

--- 5. React Router - Building Single-Page Applications (SPA)/Worldwise/src/features/app/cities/components/city-list.tsx
+++ 5. React Router - Building Single-Page Applications (SPA)/Worldwise/src/features/app/cities/components/city-list.tsx
@@ -2,9 +2,11 @@ import { useCitiesQuery } from '@/hooks/query/use-cities-query';
 import { formatDate } from '@/utils/format-date';
 import { X } from 'lucide-react';
 import { Link } from 'react-router-dom';
+import { useCurrentCity } from '../contexts/CityContext';
 
 export function CityList() {
   const { data: cities = [], isLoading, isFetching } = useCitiesQuery();
+  const { currentCity } = useCurrentCity();
 
   if (isLoading || isFetching) return <div>Loading...</div>;
 
@@ -19,7 +21,9 @@ export function CityList() {
     <div className="flex w-full flex-col space-y-4 sm:w-auto">
       {cities.map(city => (
         <Link to={`${city.id}?lat=${city.position.lat}&lng=${city.position.lng}`} key={city.id}>
-          <div className="grid grid-cols-[32px_1fr_24px] grid-rows-2 items-center gap-x-4 rounded-md border-l-4 border-l-emerald-500 bg-gray-600 px-4 py-2.5 sm:grid-cols-[32px_1fr_1fr_24px] sm:grid-rows-1 lg:grid-rows-1">
+          <div
+            className={`grid grid-cols-[32px_1fr_24px] grid-rows-2 items-center gap-x-4 rounded-md border-l-4 border-emerald-500 bg-gray-600 px-4 py-2.5 sm:grid-cols-[32px_1fr_1fr_24px] sm:grid-rows-1 lg:grid-rows-1 ${currentCity?.id === city.id ? 'border-y-2 border-r-2' : null}`}
+          >
             <picture className="row-span-full h-6 max-h-6 w-8 sm:row-span-1">
               <img
                 src={`https://flagcdn.com/${city.abbreviation}.svg`}
