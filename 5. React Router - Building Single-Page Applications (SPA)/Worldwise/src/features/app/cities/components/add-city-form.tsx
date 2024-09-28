989d24c5467c13b2ce98354045a7e95ef7f7e01e
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2024-09-28 14:55:23 +0800

    fix date parsing

--- 5. React Router - Building Single-Page Applications (SPA)/Worldwise/src/features/app/cities/components/add-city-form.tsx
+++ 5. React Router - Building Single-Page Applications (SPA)/Worldwise/src/features/app/cities/components/add-city-form.tsx
@@ -2,6 +2,7 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
+import { formatDate } from '@/utils/format-date';
 import { useState, type FormEvent } from 'react';
 import { useNavigate } from 'react-router-dom';
 
