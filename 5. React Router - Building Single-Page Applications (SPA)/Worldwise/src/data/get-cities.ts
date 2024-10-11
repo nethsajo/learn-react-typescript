12386a0e161fa88c66fc1d6a693936599b6f77c2
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2024-10-11 22:44:18 +0800

    remove unnecessary files

--- 5. React Router - Building Single-Page Applications (SPA)/Worldwise/src/data/get-cities.ts
+++
@@ -1,7 +0,0 @@
-import { BASE_URL } from '@/constants/common';
-import { type City } from '@/types/city';
-
-export async function getCitiesData(): Promise<Array<City>> {
-  const response = await fetch(`${BASE_URL}/cities`);
-  return await response.json();
-}
--- 5. React Router - Building Single-Page Applications (SPA)/Worldwise/src/hooks/query/use-cities-query.ts
+++
@@ -1,9 +0,0 @@
-import { getCitiesData } from '@/data/get-cities';
-import { useQuery } from '@tanstack/react-query';
-
-export function useCitiesQuery() {
-  return useQuery({
-    queryKey: ['cities'],
-    queryFn: getCitiesData,
-  });
-}
