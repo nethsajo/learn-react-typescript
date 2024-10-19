665e557a610d12f391ce9f3d4fd5852e52c98a16
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2024-10-19 14:44:38 +0800

    update app

--- 5. React Router - Building Single-Page Applications (SPA)/WorldWisev1/src/routes/app.tsx
+++ 5. React Router - Building Single-Page Applications (SPA)/WorldWisev1/src/routes/app.tsx
@@ -1,13 +1,11 @@
-// import { Map } from '@/features/app/map';
-import { MapVOne } from '@/features/app/map-v1';
+import { Map } from '@/features/app/map';
 import { Sidebar } from '@/features/app/sidebar';
 
 export default function AppPage() {
   return (
     <div className="flex h-full flex-col overflow-hidden lg:flex-row lg:rounded-md lg:border lg:border-gray-300">
       <Sidebar />
-      <MapVOne />
-      {/* <Map/> */}
+      <Map />
     </div>
   );
 }
