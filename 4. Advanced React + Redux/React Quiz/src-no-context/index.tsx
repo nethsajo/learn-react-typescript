4c741ca4e81910aaf605d341b35294a533721fb0
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2024-10-31 22:57:22 +0800

    remove quiz provider in index (src-no-context)

--- 4. Advanced React + Redux/React Quiz/src-no-context/index.tsx
+++ 4. Advanced React + Redux/React Quiz/src-no-context/index.tsx
@@ -3,15 +3,12 @@ import './styles.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { QuizProvider } from 'shared/contexts/quiz';
 import App from './App';
 
 const root = document.getElementById('root') as HTMLElement;
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <QuizProvider>
-      <App />
-    </QuizProvider>
+    <App />
   </React.StrictMode>
 );
