0f39426b2937723e5a06c988ab6128fa2d0892ef
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2025-01-31 23:04:17 +0800

    update root layout and add cart icon

--- 8. Fast React Pizza/src/components/partials/navbar.tsx
+++ 8. Fast React Pizza/src/components/partials/navbar.tsx
@@ -1,19 +1,28 @@
 import { ROUTES } from '@/constants/routes';
 import { SearchOrder } from '@/features/order/_components/search-order';
-import { Pizza } from 'lucide-react';
+import { Pizza, ShoppingCart } from 'lucide-react';
 import { Link } from 'react-router-dom';
 
 export function Navbar() {
   return (
     <div className="fixed inset-x-0 top-0 z-10 border-b border-b-gray-300">
       <div className="bg-red-700">
-        <div className="flex h-14 items-center px-4 sm:px-6">
+        <div className="container mx-auto flex h-14 items-center px-4 sm:px-6">
           <Link to={ROUTES.HOME} className="flex items-center space-x-1 text-red-50">
             <Pizza />
             <span className="text-lg font-medium tracking-wide">OvenFreshPizza</span>
           </Link>
-          <div className="ml-auto">
+          <div className="ml-auto flex items-center space-x-4 self-stretch">
             <SearchOrder />
+            <Link
+              to={ROUTES.CART}
+              className="relative inline-flex h-full items-center px-2 text-red-50"
+            >
+              <ShoppingCart className="h-5 w-5" />
+              <span className="absolute right-0.5 top-3 flex h-4 w-4 items-center justify-center rounded-full bg-red-200 text-xs font-bold text-red-600">
+                1
+              </span>
+            </Link>
           </div>
         </div>
       </div>
