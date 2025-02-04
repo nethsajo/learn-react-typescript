e1378d417c46a3565e39c6ae719f2961964196e8
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2025-02-04 22:10:30 +0800

    add cart button

--- 8. Fast React Pizza/src/features/menu/_components/menu-item.tsx
+++ 8. Fast React Pizza/src/features/menu/_components/menu-item.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button';
 import { formatCurrency } from '@/utils/format-currency';
 import { type Menu } from '../_types/menu';
 
@@ -13,14 +14,24 @@ export const MenuItem = ({ menu }: MenuItemProp) => {
         alt={`${menu.name} photo`}
         className={`aspect-square w-full self-start rounded-md ${menu.soldOut && 'opacity-80 grayscale'}`}
       />
-      <div
-        className={`flex flex-col space-y-2 ${!menu.soldOut ? 'text-zinc-700' : 'text-zinc-500'}`}
-      >
-        <p className="text-lg font-semibold  md:text-xl">{menu.name}</p>
-        <p className="capitalize text-zinc-500">{menu.ingredients.join(', ')}</p>
-        <div className="">
-          {!menu.soldOut ? <p>{formatCurrency(menu.unitPrice)}</p> : <p>Sold out</p>}
+      <div className="flex flex-col justify-between">
+        <div
+          className={`flex flex-col space-y-2 ${!menu.soldOut ? 'text-zinc-800' : 'text-zinc-500'}`}
+        >
+          <div className="flex items-center">
+            <p className="flex-1 text-lg font-semibold sm:text-xl">{menu.name}</p>
+            <p className="font-medium uppercase sm:text-base">
+              {!menu.soldOut ? formatCurrency(menu.unitPrice) : 'Sold out'}
+            </p>
+          </div>
+          <p className="capitalize text-zinc-500">{menu.ingredients.join(', ')}</p>
         </div>
+        <Button
+          className="w-min border-red-500 text-zinc-800 hover:bg-red-500 active:bg-red-600"
+          variant="outline"
+        >
+          Add to cart
+        </Button>
       </div>
     </div>
   );
