d5793d4e0261f00807f12b34b1dd8f2278f51e12
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2025-02-07 22:00:42 +0800

    update button component style

--- 8. Fast React Pizza/src/components/ui/button.tsx
+++ 8. Fast React Pizza/src/components/ui/button.tsx
@@ -8,21 +8,22 @@ export interface ButtonProps
 }
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-900 focus-visible:ring-offset-2 focus-visible:ring-offset-white disabled:cursor-not-allowed disabled:pointer-events-none disabled:opacity-50 transition-colors',
+  'inline-flex gap-2 items-center justify-center whitespace-nowrap rounded-md text-sm font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-900 focus-visible:ring-offset-2 focus-visible:ring-offset-white disabled:cursor-not-allowed disabled:pointer-events-none disabled:opacity-50 transition-colors',
   {
     variants: {
       variant: {
-        default: 'bg-indigo-500 text-white hover:bg-indigo-600 active:bg-indigo-700',
-        destructive: 'bg-red-500 text-white hover:bg-red-600 active:bg-red-700',
-        outline: 'border border-gray-500 hover:bg-gray-500 hover:text-white',
-        secondary: 'bg-gray-200 hover:bg-gray-300 hover:text-gray-700',
-        ghost: 'hover:bg-gray-500/70 hover:text-white',
-        link: 'text-white hover:underline underline-offset-4',
+        default: 'bg-zinc-900 text-white hover:bg-zinc-900/90',
+        destructive: 'bg-red-500 text-white hover:bg-red-500 hover:opacity-90',
+        outline: 'border border-zinc-200 hover:bg-zinc-100 text-zinc-900',
+        secondary: 'bg-zinc-100 hover:bg-zinc-100/80 text-zinc-900',
+        ghost: 'hover:bg-zinc-100 text-zinc-900',
+        link: 'text-zinc-900 hover:underline underline-offset-4',
       },
       size: {
-        sm: 'h-9 px-4',
-        md: 'h-12 px-4',
-        lg: 'h-14 px-6',
+        sm: 'h-9 px-4 py-2',
+        md: 'h-12 px-4 py-2',
+        lg: 'h-14 px-6 py-3',
+        icon: 'h-9 w-9',
       },
     },
     defaultVariants: {
