a03cb610ceb18fe3c6ec558bb84b961078de4924
Jan Kenneth Sajo <jankennethsajo@gmail.com>
2025-04-01 16:19:34 +0800

    update button

--- 8. Fast React Pizza/src/components/ui/button.tsx
+++ 8. Fast React Pizza/src/components/ui/button.tsx
@@ -8,14 +8,14 @@ export interface ButtonProps
 }
 
 const buttonVariants = cva(
-  'inline-flex gap-2 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 [&_svg]:stroke-current items-center justify-center whitespace-nowrap rounded-md text-sm font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-900 focus-visible:ring-offset-2 focus-visible:ring-offset-white disabled:cursor-not-allowed disabled:pointer-events-none disabled:opacity-50 transition-colors',
+  'inline-flex gap-2 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 [&_svg]:stroke-current items-center justify-center whitespace-nowrap rounded-md text-sm font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-900 focus-visible:ring-offset-2 focus-visible:ring-offset-white disabled:cursor-not-allowed disabled:opacity-50 transition-colors',
   {
     variants: {
       variant: {
-        default: 'bg-zinc-900 text-white hover:bg-zinc-900/90',
-        destructive: 'bg-red-500 text-white hover:bg-red-500 hover:opacity-90',
+        default: 'bg-zinc-900 text-white hover:opacity-90',
+        destructive: 'bg-red-500 text-white hover:opacity-90',
         outline: 'border border-zinc-200 hover:bg-zinc-100 text-zinc-900',
-        secondary: 'bg-zinc-100 hover:bg-zinc-100/80 text-zinc-900',
+        secondary: 'bg-zinc-100 hover:opacity-90 text-zinc-900',
         ghost: 'hover:bg-zinc-100 text-zinc-900',
         link: 'text-zinc-900 hover:underline underline-offset-4',
       },
